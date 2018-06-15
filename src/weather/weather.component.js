06c238393a04e2d0ecda19411fc0dec2425aae22
Andy Evans <andrewevans0@outlook.com>
2018-06-15 18:48:53 +0100

    added stuff

--- src/weather/weather.component.js
+++ src/weather/weather.component.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import './weather.css';
+import { Observable } from 'rxjs';
+
+let dataStore;
 
 class Weather extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pictures: [],
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    fetch('https://us-central1-userddata.cloudfunctions.net/helloWorld/weatherdata').then(res => {
+      return res.json();
+    }).then(data => {
+      dataStore = data;
+      let observed = Observable.create(data);
+      observed.subscribe(d => console.log(d));
+      let test = JSON.stringify(data);
+      this.setState({datastate: test});
+      // console.log(data)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        weather is here!
+        weather is here! {this.state.datastate}
       </div>
     );
   }
 }
+
+
 
 export default Weather;
