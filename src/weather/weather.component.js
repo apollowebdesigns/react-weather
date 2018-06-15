4586919aab1e176d774b8944863636c9c84501da
Andy Evans <andrewevans0@outlook.com>
2018-06-15 17:53:52 +0100

    init react app

---
+++ src/weather/weather.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './weather.css';
+
+class Weather extends Component {
+  render() {
+    return (
+      <div className="App">
+        weather is here!
+      </div>
+    );
+  }
+}
+
+export default Weather;
