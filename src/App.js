c15c62bce63d6c0ca76d51e81d87be262d77abf1
Andy Evans <andrewevans0@outlook.com>
2018-06-25 09:55:05 +0100

    added new feature test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React with CI</h1>
         </header>
         <Weather /> 
         <p className="App-intro">
