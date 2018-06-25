5668c96e273ebc588464859f1149f35bec382021
Andy Evans <andrewevans0@outlook.com>
2018-06-25 09:20:53 +0100

    updated travis deploy file

--- src/widget/widget.component.js
+++ src/widget/widget.component.js
@@ -61,7 +61,7 @@ class Widget extends Component {
         <button className="button">Hello!</button>
         <div className="card sizers">
           <div className="card-divider">
-            <h4>Weather Widget just deployed</h4>
+            <h4>Weather Widget just</h4>
           </div>
           <img src="http://downloadicons.net/sites/default/files/cloud-logo-icon-22859.png" />
           <div className="card-section">
