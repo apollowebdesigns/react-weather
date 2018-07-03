ec13c8e9fa3b73d135e4cf8310792445e0b5c005
Andy Evans <andrewevans0@outlook.com>
2018-07-03 21:41:33 +0100

    added charts to app

--- src/weather/weather.component.js
+++ src/weather/weather.component.js
@@ -1,5 +1,48 @@
 import React, { Component } from 'react';
 import './weather.css';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import ReactFC from 'react-fusioncharts';
+
+Charts(FusionCharts);
+
+let myDataSource = {
+  chart: {
+    caption: 'Temperatures in the flat',
+    subCaption: 'Top 5 stores in last month by revenue',
+    numberPrefix: '$',
+  },
+  data: [
+    {
+      label: 'Bakersfield Central',
+      value: '880000',
+    },
+    {
+      label: 'Garden Groove harbour',
+      value: '730000',
+    },
+    {
+      label: 'Los Angeles Topanga',
+      value: '590000',
+    },
+    {
+      label: 'Compton-Rancho Dom',
+      value: '520000',
+    },
+    {
+      label: 'Daly City Serramonte',
+      value: '330000',
+    },
+  ],
+};
+
+let chartConfigs = {
+  type: 'column2d',
+  width: 600,
+  height: 400,
+  dataFormat: 'json',
+  dataSource: myDataSource,
+};
 
 class Weather extends Component {
   constructor() {
@@ -15,12 +58,15 @@ class Weather extends Component {
       return res.json();
     }).then(data => {
       this.setState({datastate: JSON.stringify(data)});
+      myDataSource.data.forEach(value => value.value = "100000");
+      this.setState({ isLoading: false });
     })
   }
 
   render() {
     return (
       <div className="App">
+        <ReactFC {...chartConfigs} />
         weather is here! {this.state.datastate}
       </div>
     );
