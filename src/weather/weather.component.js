e1dfdbd7f8238eb8db00f08e2009f506f13244b3
Andy Evans <andrewevans0@outlook.com>
2018-07-06 19:35:43 +0100

    added responsive design to chart

--- src/weather/weather.component.js
+++ src/weather/weather.component.js
@@ -44,14 +44,16 @@ let chartConfigs = {
 };
 
 class Weather extends Component {
-  constructor() {
-    super();
-    this.state = {
-      pictures: [],
-    }
+  constructor(props) {
+    super(props);
+    this.state = { width: 0, height: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
   }
 
   componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+
     this.setState({ isLoading: true });
     fetch('https://us-central1-userddata.cloudfunctions.net/helloWorld/weatherdata').then(res => {
       return res.json();
@@ -82,7 +84,17 @@ class Weather extends Component {
       //   item.value = "100000";
       // });
       this.setState({ isLoading: false });
+      chartConfigs.width = this.state.width;
+      this.setState({ isLoading: true });
     })
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
   }
 
   render() {
