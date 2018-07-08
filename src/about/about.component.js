e3dd8714635d92575a55b0bef1f2707ab1abf97d
Andy Evans <andrewevans0@outlook.com>
2018-07-08 17:32:54 +0100

    updated images

--- src/about/about.component.js
+++ src/about/about.component.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './about.css';
+import logo from './IMG_0756.JPG';
 
 class About extends Component {
-  render() {
+  render() {  
     return (
       <div className="App">
-        This is the about page!
+        What is the weather in the flat?
+        <div>
+          <img src={logo} className="pi-image" alt="logo" />
+        </div>
       </div>
     );
   }
